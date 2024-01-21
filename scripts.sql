caf98dab07c78796ccaae616afa6fc480a248f94
Inga <55615906+Inga-Z@users.noreply.github.com>
2024-01-21 20:50:31 +0300

    Update scripts.sql

--- scripts.sql
+++ scripts.sql
@@ -1,52 +1,55 @@
--- Активные пользовательские соединения
-SELECT  @@Servername AS Server ,
-        DB_NAME(database_id) AS DatabaseName ,
-        COUNT(database_id) AS Connections ,
-        Login_name AS  LoginName ,
-        MIN(Login_Time) AS Login_Time ,
-        MIN(COALESCE(last_request_end_time, last_request_start_time)) AS  Last_Batch
-FROM    sys.dm_exec_sessions
-WHERE   database_id > 0
-        AND DB_NAME(database_id) NOT IN ( 'master', 'msdb' )
-GROUP BY database_id ,
-         login_name
-ORDER BY DatabaseName;
+-- информация о кластере postgresql
+pg_lsclusters
+Ver Cluster Port Status Owner    Data directory              Log file
+14  main    5432 online postgres /var/lib/postgresql/14/main /var/log/postgresql/postgresql-14-main.log
 
---проверить % выполнение команд и сколько времени до завершения
-select	r.session_id as SPID, 
-		command, 
-		a.text as query, 
-		start_time, 
-		percent_complete,
-		dateadd(second, estimated_completion_time/1000, GETDATE()) as estimated_completion_time
-from sys.dm_exec_requests r
-cross apply sys.dm_exec_sql_text(r.sql_handle) a
-where r.command in ('BACKUP DATABASE', 'RESTORE DATABASE', 'BACKUP LOG', 'RESTORE LOG',
-					'ALTER INDEX', 'KILLED/ROLLBACK')
-					or r.command like '%DBCC%' or r.command like 'RESTORE%'
+-- команды подключения
+psql -U <user_name> -h <ip_host> -p 5432 -d <db_name> -W
+<password>
+psql -U <user_name> -h 127.0.0.1 -p 5432 -d <db_name> -W
+<password>
 
--- backup history
-select
-bs.database_name
-,[backup_type] = case bs.type when 'D' then 'full' when 'I' then 'differential' when 'L' then 'log' end
-,bs.name
-,bs.user_name
-,bs.backup_start_date
-,bs.backup_finish_date
-,bmf.physical_device_name
-,bs.is_damaged
-,bs.is_copy_only
-,bs.server_name
-,bs.machine_name
-,cast(round(bs.backup_size/1048576,2) as decimal(10,0)) as 'backup_size_mb'
-,cast(round(bs.compressed_backup_size/1048576,2) as decimal(10,0)) as 'compressed_backup_size_mb'
-from msdb.dbo.backupset bs
-left join msdb.dbo.backupmediafamily bmf on bmf.media_set_id=bs.media_set_id
-where
-bs.backup_finish_date > dateadd(dd,-1,getdate())
--- and bs.database_name in ('PADM')
--- and bs.database_name like 'Flex%'
-and bs.type = 'L' -- log backup
-or bs.type = 'D' -- full backup
--- and bs.type = 'I' -- diff backup
-order by bs.backup_finish_date desc
+-- перечитать конфигурационный файл через psql
+select pg_reload_conf();
+
+SELECT current_setting('hba_file');
+select version();
+SHOW password_encryption;
+select current_database();
+select current_role;
+SELECT * FROM information_schema.role_table_grants WHERE grantee = 'YOUR_USER':
+select * from pg_roles;
+
+--конфиги
+cd /etc/postgresql/14/main/
+cd /var/lib/postgresql/14/main
+
+nano postgresql.conf
+nano pg_hba.conf
+
+--логи
+cd /var/log/postgresql
+tail -n 10 postgresql-14-main.log
+
+service postgresql status
+service postgresql restart
+systemctl restart postgresql@14-main
+systemctl status postgresql@14-main
+
+CREATE ROLE comita_user_test WITH LOGIN PASSWORD '3B15XBI342';
+CREATE GROUP "e416_applmbfs-comita-otkazm-test-a";
+ALTER GROUP "e416_applmbfs-comita-otkazm-test-a" add user "spozdny";
+ALTER GROUP имя_группы RENAME TO новое_имя;
+ALTER USER comita_user_test WITH PASSWORD '3B15XBI342';
+DROP ROLE comita_user_test;
+GRANT pg_write_all_data TO gpavlin;
+GRANT pg_read_all_data TO gpavlin;
+REVOKE pg_write_all_data FROM gpavlin;
+REVOKE pg_read_all_data FROM gpavlin;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO myuser;
+
+CREATE DATABASE comita_otkazm_test OWNER comita_user_test;
+ALTER DATABASE _311p OWNER TO postgres;
+DROP DATABASE _311p_test;
+GRANT ALL PRIVILEGES ON DATABASE comita_otkazm_test to comita_user_test;
+ALTER DATABASE _311p_test OWNER TO comita_user_test;
